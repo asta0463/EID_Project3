db1d4cab0c01194564d8292003964a3b8e6fcf41
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-11-13 06:31:06 +0530

    Update SQS.js

--- AWS Lambda/SQS.js
+++ AWS Lambda/SQS.js
@@ -1,19 +1,14 @@
+//Code to store the incoming MQTT messages in an SQS queue along with the running average, minimum and maximum values. An intermediate queue is used to store the latest values which are fetched by the main queue before performing computations again   
 'use strict';
 
-console.log('Loading function');
+
 var AWS = require("aws-sdk");
 
 exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    //console.log('Current temperature  =', typeof(event.Temperature)); 
-    //console.log('Current Humidity =', event.Humidity);
     console.log('Count =', event.Count);
     var t=parseFloat(event.Temperature);
-    //console.log('Temp type :', typeof(t));
-    
     var h=parseFloat(event.Humidity);
     var count=parseFloat(event.Count);
-    //console.log('count type: ', typeof(count));
     var avg_temp;
     var max_temp;
     var min_temp;
@@ -39,6 +34,7 @@ exports.handler = (event, context, callback) => {
     var queueURL2= "https://sqs.us-east-1.amazonaws.com/499557241041/P3_2";
     var sqs1 = new AWS.SQS();  ////intermediate Q
     var sqs2 = new AWS.SQS();  //main
+    //if the count value is 1 , it is the first MQTT message- the average , minimum, max will all be equal to that value
     if(event.Count==1){
         avg_temp=t;
         max_temp=t;
@@ -46,140 +42,7 @@ exports.handler = (event, context, callback) => {
         avg_hum=h;
         max_hum=h;
         min_hum=h;
-        
-    
-    var myObj = { 
-        "AvgT":avg_temp, 
-        "MaxT":max_temp,
-        "MinT":min_temp, 
-        "LatestT":t,
-        "AvgH":avg_hum, 
-        "MaxH":max_hum,
-        "MinH":min_hum, 
-        "LatestH":h,
-        
-    };
-    var msg = JSON.stringify(myObj);
-    console.log("Message to send: " + msg);
-    var params = {
-    DelaySeconds: 0,
-    MessageAttributes: {
-                       "CountVal":  {
-                                    DataType: "String",
-                                    StringValue:event.Count 
-                               },
-                        },
-    MessageBody: msg,
-    QueueUrl: "https://sqs.us-east-1.amazonaws.com/499557241041/P3_QUEUE"  //intermediate queue
-    };
-    sqs1.sendMessage(params, function(err, data) {
-        if (err) {
-            console.log("Error sending msg to P3_Q", err);
-        } else {
-            console.log("Success -data sent to P3_Q",msg);
-        }
-    });
-    var params5 = {
-    DelaySeconds: 0,
-    MessageAttributes: {
-                        "CountVal": {
-                                    DataType: "String",
-                                    StringValue:event.Count 
-                                },
-                   
-                        },
-    MessageBody: msg,
-    QueueUrl: "https://sqs.us-east-1.amazonaws.com/499557241041/P3_2"
-    };
-    sqs2.sendMessage(params5, function(err, data) {
-        if (err) {
-            console.log("Error sending data to P3", err);
-        } else {
-            console.log("Success -data sent to P3 ", msg);
-        }
-});
-
-}
-else if(event.Count>1){
-    var params2 = {
-    AttributeNames: [
-    "SentTimestamp"
- ],
- MaxNumberOfMessages: 1,
- MessageAttributeNames: [
-    "All"
- ],
- QueueUrl: queueURL1,
- VisibilityTimeout: 0,
- WaitTimeSeconds: 0
-};
-
-sqs1.receiveMessage(params2, function(err, data) {
-  if (err) {
-    console.log("Receive Error", err);
-  } else {
-          console.log("msg extracted",data.Messages[0]);
-          message = data.Messages[0];
-          body = JSON.parse(message.Body);
-          console.log("msg extracted is",body);
-        avg_temp1=body.AvgT;
-        max_temp1=body.MaxT;
-        min_temp1=body.MinT;
-        avg_hum1=body.AvgH;
-        max_hum1=body.MaxH;
-        min_hum1=body.MinH;
-        //latest_temp1=body.Latest;
-        //console.log("avg type is",typeof(avg_temp1));
-        console.log("ext avg",avg_temp1);
-        console.log("ext max",max_temp1);
-        //console.log("max is",typeof(max_temp1));
-        
-        avg_tempm;
-        one=1;
-        avg_tempm=avg_temp1*(count-one);
-        
-        //console.log("middle avg is",typeof(avg_tempm));
-        //console.log("middle avg is",avg_tempm);
-
-        avgtempn=avg_tempm+t;
-        //console.log("middle middle",avgtempn);
-        //console.log("type of",typeof(avgtempn));
-        avg_temp=avgtempn/count;
-        console.log("avg is",avg_temp);
-        //console.log("avg type",typeof(avg_temp));
-        if(t>max_temp1){
-            max_temp=t;
-        }else{
-            max_temp=max_temp1;
-        }
-        if(t<min_temp1){
-            min_temp=t;
-        }else{
-            min_temp=min_temp1;
-        }
-        
-        
-        avg_humm=avg_hum1*(count-one);
-        
-        //console.log("middle hum is",typeof(avg_humm));
-        //console.log("middle hum is",avg_humm);
-        avghumn=avg_humm+h;
-        //console.log("middle middle",avghumn);
-        //console.log("type of",typeof(avghumn));
-        avg_hum=avghumn/count;
-        console.log("avg is",avg_hum);
-        //console.log("avg type",typeof(avg_hum));
-        if(h>max_hum1){
-            max_hum=h;
-        }else{
-            max_hum=max_hum1;
-        }
-        if(h<min_hum1){
-            min_hum=h;
-        }else{
-            min_hum=min_hum1;
-        }
-        var myObj2 = { 
+        var myObj = { 
             "AvgT":avg_temp, 
             "MaxT":max_temp,
             "MinT":min_temp, 
@@ -187,11 +50,30 @@ sqs1.receiveMessage(params2, function(err, data) {
             "AvgH":avg_hum, 
             "MaxH":max_hum,
             "MinH":min_hum, 
-            "LatestH":h
+            "LatestH":h,
         };
-        
-        var msg2 = JSON.stringify(myObj2);
-        var params3 = {
+        var msg = JSON.stringify(myObj);
+        console.log("Message to send: " + msg);
+        var params = {
+        DelaySeconds: 0,
+        MessageAttributes: {
+                       "CountVal":  {
+                                    DataType: "String",
+                                    StringValue:event.Count 
+                                    },
+                           },
+        MessageBody: msg,
+        QueueUrl: "https://sqs.us-east-1.amazonaws.com/499557241041/P3_QUEUE"  //intermediate queue
+        };
+        //Adding the above message in both queues
+        sqs1.sendMessage(params, function(err, data) {
+            if (err) {
+                console.log("Error sending msg to P3_Q", err);
+            } else {
+                console.log("Success -data sent to P3_Q",msg);
+            }
+        });
+        var params5 = {
         DelaySeconds: 0,
         MessageAttributes: {
                             "CountVal": {
@@ -199,47 +81,137 @@ sqs1.receiveMessage(params2, function(err, data) {
                                         StringValue:event.Count 
                                     },
                            },
-        MessageBody: msg2,
+        MessageBody: msg,
         QueueUrl: "https://sqs.us-east-1.amazonaws.com/499557241041/P3_2"
         };
-        sqs2.sendMessage(params3, function(err, data) {
+        sqs2.sendMessage(params5, function(err, data) {
             if (err) {
-                console.log("Error", err);
+                console.log("Error sending data to P3", err);
             } else {
-                console.log("Success -data send to P3 ", msg2);
+                console.log("Success -data sent to P3 ", msg);
             }
-        });
-        var params4 = {
-        DelaySeconds: 0,
-        MessageAttributes: {
-                            "CountVal": {
-                                        DataType: "String",
-                                        StringValue:event.Count 
-                                    },
-                      
-                            },
-        MessageBody: msg2,
-        QueueUrl: "https://sqs.us-east-1.amazonaws.com/499557241041/P3_QUEUE"
-        };
-        var deleteParams = {
-        QueueUrl: queueURL1,
-        ReceiptHandle: data.Messages[0].ReceiptHandle
-        };
-        sqs1.deleteMessage(deleteParams, function(err, data) {
-            if (err) {
-                console.log("Delete Error", err);
-            } else {
-                console.log("Message Deleted", data);
+         });
+}
+else if(event.Count>1){
+        var params2 = {
+        AttributeNames: [
+        "SentTimestamp"
+     ],
+     MaxNumberOfMessages: 1,
+     MessageAttributeNames: [
+        "All"
+     ],
+     QueueUrl: queueURL1,
+     VisibilityTimeout: 0,
+     WaitTimeSeconds: 0
+    };
+    //retrieve and then delete the previous avg,etc from the intermediate queue, use it to compute the running average with the 2nd value(or current) of temp/hum and store the new value of avg,etc in both queues 
+    sqs1.receiveMessage(params2, function(err, data) {
+      if (err) {
+        console.log("Receive Error", err);
+      } else {
+              console.log("msg extracted",data.Messages[0]);
+              message = data.Messages[0];
+              body = JSON.parse(message.Body);
+              console.log("msg extracted is",body);
+            avg_temp1=body.AvgT;
+            max_temp1=body.MaxT;
+            min_temp1=body.MinT;
+            avg_hum1=body.AvgH;
+            max_hum1=body.MaxH;
+            min_hum1=body.MinH;
+            one=1;
+            avg_tempm=avg_temp1*(count-one);
+            avgtempn=avg_tempm+t;
+            avg_temp=avgtempn/count;
+            console.log("avg is",avg_temp);
+            if(t>max_temp1){
+                max_temp=t;
+            }else{
+                max_temp=max_temp1;
             }
-        });
-        sqs1.sendMessage(params4, function(err, data) {
-            if (err) {
-                console.log("Error", err);
-            } else {
-                console.log("Success data sent to P3_Q", msg2);
+            if(t<min_temp1){
+                min_temp=t;
+            }else{
+                min_temp=min_temp1;
             }
-        });
-    }
-});
+
+
+            avg_humm=avg_hum1*(count-one);
+            avghumn=avg_humm+h;
+            avg_hum=avghumn/count;
+            console.log("avg is",avg_hum);
+            if(h>max_hum1){
+                max_hum=h;
+            }else{
+                max_hum=max_hum1;
+            }
+            if(h<min_hum1){
+                min_hum=h;
+            }else{
+                min_hum=min_hum1;
+            }
+            var myObj2 = { 
+                "AvgT":avg_temp, 
+                "MaxT":max_temp,
+                "MinT":min_temp, 
+                "LatestT":t,
+                "AvgH":avg_hum, 
+                "MaxH":max_hum,
+                "MinH":min_hum, 
+                "LatestH":h
+            };
+
+            var msg2 = JSON.stringify(myObj2);
+            var params3 = {
+            DelaySeconds: 0,
+            MessageAttributes: {
+                                "CountVal": {
+                                            DataType: "String",
+                                            StringValue:event.Count 
+                                        },
+                               },
+            MessageBody: msg2,
+            QueueUrl: "https://sqs.us-east-1.amazonaws.com/499557241041/P3_2"
+            };
+            sqs2.sendMessage(params3, function(err, data) {
+                if (err) {
+                    console.log("Error", err);
+                } else {
+                    console.log("Success -data send to P3 ", msg2);
+                }
+            });
+            var params4 = {
+            DelaySeconds: 0,
+            MessageAttributes: {
+                                "CountVal": {
+                                            DataType: "String",
+                                            StringValue:event.Count 
+                                        },
+
+                                },
+            MessageBody: msg2,
+            QueueUrl: "https://sqs.us-east-1.amazonaws.com/499557241041/P3_QUEUE"
+            };
+            var deleteParams = {
+            QueueUrl: queueURL1,
+            ReceiptHandle: data.Messages[0].ReceiptHandle
+            };
+            sqs1.deleteMessage(deleteParams, function(err, data) {
+                if (err) {
+                    console.log("Delete Error", err);
+                } else {
+                    console.log("Message Deleted", data);
+                }
+            });
+            sqs1.sendMessage(params4, function(err, data) {
+                if (err) {
+                    console.log("Error", err);
+                } else {
+                    console.log("Success data sent to P3_Q", msg2);
+                }
+            });
+        }
+    });
 }
 };
