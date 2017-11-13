18e6388d928a232720b8016d7294f100a8c172b2
rheabcooper <31560258+rheabcooper@users.noreply.github.com>
2017-11-13 05:52:58 +0530

    Update DynamoDB.js

--- AWS Lambda/DynamoDB.js
+++ AWS Lambda/DynamoDB.js
@@ -1,24 +1,22 @@
+//A Node.js code to populate the DynamoDB table
+//Reference : http://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/dynamodb-example-table-read-write.html
+
 'use strict';
-
-console.log('Loading function');
 var AWS = require("aws-sdk");
-
-
 // Create the DynamoDB service object
 var ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
 
-
+//Lambda function
 exports.handler = (event, context, callback) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    console.log('Current temperature  =', typeof(event.Temperature)); 
+    console.log('Received event:', JSON.stringify(event, null, 2));  //logging the input into Cloudwatch logs
+    console.log('Current temperature  =', event.Temperature)); 
     console.log('Current Humidity =', event.Humidity);
     console.log('Count =', event.Count);
-    var t=parseFloat(event.Temperature);
-    //console.log('Temp type :', typeof(t));
+    var t=parseFloat(event.Temperature);       //converting received String to object of type Float Number
     var h=parseFloat(event.Humidity);
     var count=parseFloat(event.Count);
 
-    ddb.listTables(function(err, data) {
+    ddb.listTables(function(err, data) {       //list the tables
       console.log(JSON.stringify(data, null, '  '));
     });
 var paramsdb = {
@@ -30,7 +28,7 @@ var paramsdb = {
   }
 };
 
-
+//insert into table
 ddb.putItem(paramsdb, function(err, data) {
   if (err) {
     console.log("Error", err);
